942ba998fb84cda7fa2451c144c35785bdabe645
pranav-maheshwari <pranavm.13@gmail.com>
2015-11-11 15:05:58 -0500

    Capable of serial over XBee
    Progress Review 4

--- IR/Arduino_Demonstration.py
+++ IR/Arduino_Demonstration.py
@@ -1,12 +1,21 @@
 import rospy
 from std_msgs.msg import String
+import serial
 
+ser = serial.Serial("/dev/ttyUSB0", 9600, timeout=1)
 x = rospy.init_node("sup")
+
+data = ""
+
+while data == '':
+    data = ser.readline()
+    if data == "Parking":
+        print "Car 1", data
 
 
 def callback(data):
-    print "Obstacle Detected"
-    print data.data
+    stuff = data.data
+    ser.write(stuff)
 
 
 def subscribe():
