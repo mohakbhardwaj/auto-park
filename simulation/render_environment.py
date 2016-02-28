e6a839e4dfa13c95cc090eebddbc02693ca0d114
pranav-maheshwari <pranavm.13@gmail.com>
2016-02-28 02:38:46 -0500

    Update render_environment.py

--- simulation/render_environment.py
+++ simulation/render_environment.py
@@ -8,20 +8,44 @@ from threading import Thread
 from interactive_markers.interactive_marker_server import *
 from visualization_msgs.msg import *
 
+
 # class which takes care of all the aspects of a vehicle
 class Car:
 
-
-    def __init__(self, id):
+    def __init__(self, vehicle_id, path):
         # set the color associated with this vehicle
+        self.color_r = random.randint(0, 255)
+        self.color_g = random.randint(0, 255)
+        self.color_b = random.randint(0, 255)
+        self.id = vehicle_id
+        self.vehicle_path = path
+        self.motion_start = 0
+        self.motion = 0
 
     def move(self):
         # update the state of the vehicle
+        self.motion_start = time.time()
 
-
-    def draw_path(self):
+    def draw_path(self, path):
         # draw the path of the vehicle on RViz
-
+        self.vehicle_path = path
+        self.motion = 1
 
     def clear_path(self):
         # clear the path of the vehicle
+        self.vehicle_path = []
+        self.motion_start = 0
+        self.motion = 0
+
+    def clear(self):
+        # publish a message to remove the vehicle
+
+
+vehicles = []
+
+for i in range(0, 10):
+    a = Car(i, [])
+    vehicles.append(a)
+
+# have a custom message of line, two markers, and a cube in each object of the class
+# keep on updating the positions based on motion flag
