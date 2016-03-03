82514076ead3c4b6c049ff93303778fa537bb840
pranav-maheshwari <pranavm.13@gmail.com>
2016-03-03 13:19:22 -0500

    Update backend.py
    Custom message

--- simulation/backend.py
+++ simulation/backend.py
@@ -9,7 +9,7 @@ from simulation.msg import *
 
 rospy.init_node("Backend")
 command = rospy.Publisher("simulation_backend", commands, queue_size=19)
-
+msg = commands()
 mean_time = 60
 deviation_stay = 30
 deviation_arrival = 20
@@ -19,6 +19,7 @@ cars_arrival = np.array([sum(cars_arrival_time[0:off]) for off in range(1, len(c
 cars_stay_time = [round(elem, 1) for elem in np.random.normal(mean_time, deviation_stay, 200)]
 cars_stay = np.array(np.sum([sum(cars_stay_time[0:off]) for off in range(1, len(cars_stay_time))], cars_arrival, 0))
 cars_size = [randint(1, 3) for _ in range(0, 200)]
+ids = np.array(xrange(0, 200))
 
 time_init = time.time()
 
@@ -28,9 +29,16 @@ while True:
     remove_index = []
     for i in range(0, len(cars_arrival)):
         if time_off > cars_arrival[i]:
+            msg.state = "arrive"
+            msg.id = ids[i]
             print "Publish a car"
+            command.publish(msg)
         elif time_off > cars_stay[i]:
+            msg.state = "return"
+            msg.id = ids[i]
             print "Remove a car"
+            command.publish(msg)
             remove_index.append(i)
+    ids = np.delete(ids, remove_index, None)
     cars_arrival = np.delete(cars_arrival, remove_index, None)
     cars_stay = np.delete(cars_stay, remove_index, None)
