a11dcd36e23b45570c5da76402d7e1bc87fbc490
mohak <mohak.bhardwaj@gmail.com>
2016-04-09 00:03:05 -0400

    localplanner not working

--- simulation/localplanner/src/statespace_planner.cpp
+++ simulation/localplanner/src/statespace_planner.cpp
@@ -4,6 +4,7 @@
 #include <queue>
 #include <sstream>
 #include <cstdlib>
+#include <cmath>
 //ROS
 #include <ros/ros.h>
 #include <std_msgs/String.h>
@@ -27,6 +28,7 @@
 //TODO: Make different class for lattice_tree with Node types, generated states etc.
 //TODO: Add costMap2D from ROS 
 const double PI  =3.141592653589793238463;
+
 //Struct for Nodes in A* tree traversal
 struct Pose
 {
@@ -76,7 +78,8 @@ void openFile(std::ifstream&, std::string);
 void LoadEnvironment(std::string, std::string );
 void LoadCostMap(std::vector<std::vector<int> >&, std::string);
 void LoadParkingSpots(std::map<Pose, std::string, pose_cmp>&, std::string);
-
+int checkCollision(const Pose&); //Return collision cost value
+int extendCheckCollision(const Pose& , double , double, double );
 bool processpathQuery(localplanner::optimPath::Request&, localplanner::optimPath::Response&);
 bool processCostQuery(localplanner::spotsTreadCost::Request& , localplanner::spotsTreadCost::Response&);
 double astarstatespace(std::pair<Pose,Pose>, std::vector<geometry_msgs::PoseStamped>&);
@@ -94,19 +97,41 @@ double h1(const Pose& p1, const Pose& p2);
 
 int main(int argc, char **argv)
 {
-	LoadEnvironment("/home/pranav/catkin_ws/src/localplanner/env_files/env.txt", "../env_files/parking_spot.txt");
+	LoadEnvironment("/home/mohak/catkin_ws/src/auto-park/simulation/localplanner/env_files/env.txt", "/home/mohak/catkin_ws/src/auto-park/simulation/localplanner/env_files/parking_spot.txt");
 	//ROS node functionality
 	ros::init(argc, argv, "statespace_planner");
 	ros::NodeHandle n;
 	ros::Rate loop_rate(100);
+	//debug print of costmap
+	/*for(size_t i = 0; i < env.costmap.size(); ++i)
+	{
+		for(size_t j =0; j < env.costmap[i].size(); ++j)
+		{	std::cout << env.costmap[i][j];
+
+		}
+		std::cout << std::endl;
+	}*/
+	//std::cout << env.costmap.size() << " " << env.costmap[0].size() << std::endl;
+	/*for(size_t j = 0; j < env.costmap[0].size(); ++j)
+	{	
+		std::cout << env.costmap[0][j] << " ";
+
+	}*/
+	/*Pose s;
+	Pose g;
+	s.x = 2.5;
+	s.y = 2.0;
+	s.th = PI/4;
+
+	g.x = 37; g.y = 25; g.th = PI/4;
+	std::vector<geometry_msgs::PoseStamped> path;
+	double dd =  astarstatespace(std::make_pair(s,g),path);*/
+
+
 
 	//ROS services setup
 	ros::ServiceServer costservice = n.advertiseService("spotsTreadCost", processCostQuery);
 	ros::ServiceServer pathservice = n.advertiseService("optimPath", processpathQuery);
-
-		
-		
-	std::cout << "Running...Running...Running" << std::endl;
 	
 	loop_rate.sleep();
 
@@ -129,6 +154,7 @@ void openFile(std::ifstream& file, std::string filename)
 		{
 			std::cerr<<"No such file exists with name "<< filename << std::endl;
 			file.clear();
+			ros::Duration(0.1).sleep(); //sleep coz blaaahhhhh
 		} 
 
 	}
@@ -218,9 +244,35 @@ void end_pose(const Pose& startpose, Pose& endpose, double curvature, double len
 
 	}
 }
+//Check in the raster or costmap for the value of collision magnitude
+int checkCollision(const Pose& p)
+{	
+	Pose lp; 
+	lattice_pose(p, lp, env.raster_size);
+	return env.costmap[env.costmap.size()-lp.y-1][lp.x];
+}
+int extendCheckCollision(const Pose& startpose, double curvature, double length, double dl)
+{
+	double l = 0.0;
+	dl = copysign(dl, length);
+	int coll_val = 0;
+	//std::cout << "l = " << l << " length = " << length << " dl = " << dl <<  std::endl;
+	while(abs(l) < length)
+	{
+		Pose e;
+		end_pose(startpose, e, curvature, l);
+	    coll_val += checkCollision(e);
+		
+		l += dl;
+
+	}
+	//std::cout << coll_val << std::endl;
+	return coll_val;
+
+}
 void get_movements(std::vector<std::pair<double,double> >& movements)
 {	//Each movement is of the type (curvature,distance)
-	double distance = 5.0;
+	double distance = .05;
 	double curvature_min = 0.0;
 	double curvature_max = 1.0/10;
 	movements.push_back(std::make_pair(curvature_max, distance));
@@ -229,10 +281,16 @@ void get_movements(std::vector<std::pair<double,double> >& movements)
 	movements.push_back(std::make_pair(curvature_max, -distance));
 	movements.push_back(std::make_pair(curvature_min, -distance));
 	movements.push_back(std::make_pair(-curvature_max, -distance));
+	movements.push_back(std::make_pair(2.0*curvature_max, distance));
+	movements.push_back(std::make_pair(2.0*curvature_min, distance));
+	movements.push_back(std::make_pair(-2.0*curvature_max, distance));
+	//movements.push_back(std::make_pair(3.0*curvature_max, -distance));
+	//movements.push_back(std::make_pair(3.0*curvature_min, -distance));
+	//movements.push_back(std::make_pair(-3.0*curvature_max, -distance));
 }
 void lattice_pose(const Pose& continuouspose, Pose& latticepose, double pos_raster)
 {	//Given a pose returns a discrete pose in lattice
-	double heading_raster = rad(10.0);
+	double heading_raster = rad(1);
 	latticepose.x = (floor(continuouspose.x/pos_raster));
 	latticepose.y = (floor(continuouspose.y/pos_raster));
 	latticepose.th = (floor(continuouspose.th/heading_raster));
@@ -253,8 +311,8 @@ bool poses_close(const Pose& p1, const Pose& p2)
 
 	double d_angle = abs(remainder(p1.th - p2.th + PI, (2.0*PI)) - PI);
 
-	return d_angle < rad(10.0) && distance_euclidean(p1,p2) <= 2.0;
-
+	return d_angle < rad(15.0) && distance_euclidean(p1,p2) <= 2.0;
+	//return distance_euclidean(p1,p2) <= 2.0;
 }
 
 double rad(double angle_in_degrees)
@@ -271,7 +329,7 @@ bool processpathQuery(localplanner::optimPath::Request &req, localplanner::optim
 	Pose start,goal;
 	PoseStampedtoPose(req.query[0], start);
 	PoseStampedtoPose(req.query[1], goal);
-	ROS_INFO("Received path query from [%f, %f] to [%f, %f]", start.x, start.y, goal.x, goal.y);
+	ROS_INFO("Received path query from rendering engine for [%f, %f] to [%f, %f]", start.x, start.y, goal.x, goal.y);
 	astarstatespace(std::make_pair(start,goal), res.path);
 	return true;
 }
@@ -281,7 +339,7 @@ bool processCostQuery(localplanner::spotsTreadCost::Request &req, localplanner::
 	Pose start,goal;
 	PoseStampedtoPose(req.query[0], start);
 	PoseStampedtoPose(req.query[1], start);
-	ROS_INFO("Received cost query from [%f, %f] to [%f, %f]", start.x, start.y, goal.x, goal.y);
+	ROS_INFO("Received cost query from global planner for [%f, %f] to [%f, %f]", start.x, start.y, goal.x, goal.y);
 	std::vector<geometry_msgs::PoseStamped> path;
 	res.pathcost = astarstatespace(std::make_pair(start,goal), path);
 
@@ -328,12 +386,19 @@ double astarstatespace(std::pair<Pose,Pose> query, std::vector<geometry_msgs::Po
 	std::vector<std::pair<double,double> >movements;
 	get_movements(movements);
 	Node *curr_node;
+	if(checkCollision(goal_pose) == 255)
+	{
+		ROS_WARN("Invalid query: goal in collison. Returning empty path");
+		path.clear();
+		return -1;
+
+	}
 	while(open.size() > 0)
 	{	//Stop search if frontier gets too large
 		
 		if(open.size() > 500000)
 		{
-			ROS_WARN("Timeout: Did not find feasible path for start and goal");
+			
 			break;
 		}
 		//Pop the best node from the frontier
@@ -354,12 +419,14 @@ double astarstatespace(std::pair<Pose,Pose> query, std::vector<geometry_msgs::Po
 		//Check if reached goal
 		if(poses_close(curr_node->p, goal_pose))
 		{
+			std::cout << curr_node->p.x << " " << curr_node->p.y << std::endl;
 			path_cost = curr_node->g;	
+			
 			break; //Finished
 		}
 
 		//Expand the current node
-		for(int i =0; i < movements.size(); ++i)
+		for(int i = 0; i < movements.size(); ++i)
 		{
 			Node *new_node = new Node;
 			double curvature = movements[i].first;
@@ -371,9 +438,16 @@ double astarstatespace(std::pair<Pose,Pose> query, std::vector<geometry_msgs::Po
 				continue;
 			}
 			Pose new_lattice_pose;
-			lattice_pose(new_node->p, new_lattice_pose, pose_raster);
-			if(env.costmap[new_lattice_pose.x][new_lattice_pose.y] != 255)
+			lattice_pose(new_node->p, new_lattice_pose,pose_raster);
+			//std::cout << new_lattice_pose.x << " " << new_lattice_pose.y << std::endl;
+			//std::cout << env.costmap[new_lattice_pose.y][new_lattice_pose.x] << std::endl;
+			/*if(generated_states.count(new_lattice_pose))
 			{
+				continue;
+			}*/
+			
+			if(checkCollision(new_node->p)!= 255 && extendCheckCollision(curr_node->p, curvature, length, pose_raster) < 255)
+			{	//std::cout << extendCheckCollision(curr_node->p, curvature, length, pose_raster)<< std::endl;
 				new_node->g = curr_node->g + abs(length);
 				double new_h = h1(new_node->p,goal_pose); //+ ...
 				new_node->f = new_node->g + new_h;
@@ -403,11 +477,24 @@ double astarstatespace(std::pair<Pose,Pose> query, std::vector<geometry_msgs::Po
 		PosetoPoseStamped(goal_push, goal_pose);
 		path.insert(path.begin(),start_push);	
 		path.push_back(goal_push);
+		ROS_INFO("Path returned");
 	}
 	else
 	{
-		ROS_WARN("Returning empty path");
+		
+		if(open.size() == 0)
+		{
+			ROS_WARN("Exhausted all options. Returning empty path");
+		}
+		else if(open.size() > 500000)
+		{
+			ROS_WARN("Timeout: No path found. Returning empty path");
+		}
+
+		
+		
 		path.clear();
+		return -1;
 	}
 	/*for(size_t i = 0; i < path.size(); ++i)
 	{
